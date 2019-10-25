53dc02a7ca9ed7e0ee2354bcb025e855478a6c22
krcdavis <32370620+krcdavis@users.noreply.github.com>
2019-10-25 09:31:59 -0700

    Add files via upload
    moved some functions to a header file to clean up the main file

---
+++ lexer-2.h
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+//=========================================================================
+// Function to check if a key value matches a value in an array
+//=========================================================================
+bool in_array(char key, char* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		if (key == arr[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+//=========================================================================a
+// Function to check if a number is an integer or a real number
+//=========================================================================
+string getNumOrReal(char number) {
+
+	//====================================================================
+	//Finite state machine that can tell if int or real number or invalid
+	// States:
+	//		1 - Integer
+	//		2 - Real
+	//		3 - Invalid
+	//====================================================================
+	int finiteStateMachine[4][3] = {
+				/*Initial State 0*/	 {1, 3, 3},
+				/*State 1*/		 {1, 2, 3},
+				/*State 2*/		 {2, 3, 3},
+				/*State 3*/		 {3, 3, 3}
+									};
+
+
+	//====================================================================
+	// Pointer keeping track of position in finite state machine
+	// Starts at State 1 because we already know the first char is a number
+	//====================================================================
+	int position = 1;
+
+	char buf[100];
+	buf[0] = number;
+	int index = 1;
+
+	char nextChar;
+	while (nextChar = fin.peek()) {
+		if (isdigit(nextChar)) {
+			position = finiteStateMachine[position][0];
+		}
+		else if (nextChar == '.') {
+			position = finiteStateMachine[position][1];
+		}
+		else {
+			position = finiteStateMachine[position][2];
+		}
+
+		if (position == 3) {
+			buf[index] = '\0';
+			break;
+		}
+		else {
+			buf[index++] = nextChar;
+		}
+		fin.get();
+	}
+	return buf;
+}
+
+string isKeyword(char buffer[]) {
+	string keywords[14] = { "real", "int", "function", "if", "fi", "for", "while", "return", "get", "put", "otherwise", "boolean", "true", "false" };
+	int i, flag = 0;
+
+	for (i = 0; i < 14; ++i) {
+		if (strcmp(keywords[i].c_str(), buffer) == 0) {
+			return keywords[i];
+		}
+	}
+	return "-1";
+}
