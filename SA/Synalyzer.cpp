7c9219425534963e5c7aafecd15c7dbc51b2f2fb
John S <tinco7@aol.com>
2019-11-11 00:51:39 -0800

    sadsad

--- SA/Synalyzer.cpp
+++ SA/Synalyzer.cpp
@@ -3,45 +3,6 @@
 using namespace std;
 ifstream src("./output.txt");
 
-//class that holds lexeme and token for each line
-//and has member functions to get them
-/*
-class input{
-public:
-  string token;
-  string lexeme;
-  string gettoken(){
-    return this->token;
-  }
-  string getlexeme(){
-    return this->lexeme;
-  }
-};
-string token;
-string lexeme;
-string curLine;
-//vector that holds all of the input lines defined above
-vector<input> inputs;
-//function that puts the output of the lexer into an easy to traverse vector.
-void getInputs(){
-  string token;
-  string lexeme;
-  input temp;
-  while (!src.eof()){
-    src >> token;
-    src >> lexeme;
-    temp.token = token;
-    temp.lexeme = lexeme;
-    inputs.push_back(temp);
-  }
-  temp.token = "$";
-  temp.lexeme = "$";
-  inputs.push_back(temp);
-  for (input i : inputs){
-    cout << i.token << " ----- " << i.lexeme << endl;
-  }
-}
-
 //Beginning of rules
 void APrime(){//the rule that augments the grammar. Just calls A. Not sure if it needs to check anything.
   if (A()){
@@ -313,7 +274,7 @@ void BB(){ //may have to rewrite this one in proper order.
     return true;
   }
 }
-*/
+
 void CC(){
   LexerDone();
 }
