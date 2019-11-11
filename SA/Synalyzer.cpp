96542a36ceaebff1c9afe08a064b0d271391ce2f
John S <tinco7@aol.com>
2019-11-11 12:45:21 -0800

    almost done

--- SA/Synalyzer.cpp
+++ SA/Synalyzer.cpp
@@ -1,8 +1,250 @@
-#include "lexer.h"
-#include <vector>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string>
+#include <string.h>
+//#include "lexer.h"
 using namespace std;
-ifstream src("./output.txt");
+ofstream fout("./output.txt");
+ifstream fin("../Lexer/Test-Cases/sample.rat19");
+char ch2;
+char ch;
+string ch3;
+string keyword;
+string identifier;
+string number;
+void CC();
+bool BB();
+bool AA();
+bool ZPrime();
+bool Z();
+bool YPrime();
+bool Y();
+bool X();
+bool W();
+bool V();
+bool U();
+bool T();
+bool S();
+bool R();
+bool Q();
+bool P();
+bool O();
+bool N();
+bool M();
+bool L();
+bool K();
+bool J();
+bool I();
+bool H();
+bool G();
+bool F();
+bool E();
+bool D();
+bool C();
+bool B();
+bool A();
+bool APrime();
 
+//=========================================================================
+// Function to check if a key value matches a value in an array
+//=========================================================================
+bool in_array(char key, char* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		if (key == arr[i]) {
+			return true;
+		}
+	}
+	return false;
+}
+//=========================================================================a
+// Function to check if a number integer or a real number
+//=========================================================================
+string getNumOrReal(char number) {
+
+	//====================================================================
+	//Finite state machine that can tell if int or real number or invalid
+	// States:
+	//		1 - Integer
+	//		2 - Real
+	//		3 - Invalid
+	//====================================================================
+	int finiteStateMachine[4][3] = {
+				/*Initial State 0*/{1, 3, 3},
+				/*State 1*/		     {1, 2, 3},
+				/*State 2*/        {2, 3, 3},
+				/*State 3*/		     {3, 3, 3}
+									};
+
+
+	//====================================================================
+	// Pointer keeping track of position in finite state machine
+	// Starts at State 1 because we already know the first char is a number
+	//====================================================================
+	int position = 1;
+
+	char buf[100];
+	buf[0] = number;
+	int index = 1;
+
+	char nextChar;
+	while (nextChar = fin.peek()) {
+		if (isdigit(nextChar)) {
+			position = finiteStateMachine[position][0];
+		}
+		else if (nextChar == '.') {
+			position = finiteStateMachine[position][1];
+		}
+		else {
+			position = finiteStateMachine[position][2];
+		}
+
+		if (position == 3) {
+			buf[index] = '\0';
+			break;
+		}
+		else {
+			buf[index++] = nextChar;
+		}
+		fin.get();
+	}
+	return buf;
+}
+
+string isKeyword(char buffer[]) {
+	string keywords[14] = { "real", "int", "function", "if", "fi", "for", "while", "return", "get", "put", "otherwise", "boolean", "true", "false" };
+	int i, flag = 0;
+
+	for (i = 0; i < 14; ++i) {
+		if (strcmp(keywords[i].c_str(), buffer) == 0) {
+			return keywords[i];
+		}
+	}
+	return "-1";
+}
+
+string Lexer() {
+	if (!fin.is_open()) {
+		cout << "error while opening the file\n";
+		exit(0);
+	}
+	while (ch != ' ' || ch != '\n'){
+		ch = fin.get();
+		char operators[8]= { '+', '-', '/', '*', '=', '<', '!' };
+		char seperators[10] = { '%', ')', '(', ';', '{', '}', '[', ',', ']', '*' };
+
+		if (isalpha(ch)) {
+			int finiteStateMachine[4][3] = {
+						/*Initial State 0*/{1, 3, 3},
+						/*State 1 id*/		 {1, 2, 3},
+						/*State 2 key*/		 {2, 3, 3},
+						/*State 3 inv*/		 {3, 3, 3}
+			};
+			int index = 0;
+			char identifier[100];
+			identifier[0] = ch;
+			ch2 = fin.peek();
+			while (isalnum(ch2) || ch2 == '_') {
+				index++;
+				identifier[index] = ch2;
+				fin.get();
+				ch2 = fin.peek();
+			}
+			identifier[++index] = '\0';
+			keyword = isKeyword(identifier);
+			if (keyword != "-1") {
+				cout << keyword << " keyword\n";
+				fout << keyword << " keyword\n";
+				return "keyword";
+			}
+			else {
+				cout << identifier << " identifier\n";
+				fout << identifier << " identifier\n";
+				return "identifier";
+			}
+		}
+
+		else if (isdigit(ch)) {
+			number = getNumOrReal(ch);
+			if (number.find(".") != -1) {
+				cout << number << " real\n";
+				fout << number << " real\n";
+				return "real";
+			}
+			else {
+				cout << number << " integer\n";
+				fout << number << " integer\n";
+				return "integer";
+			}
+		}
+
+		else if (in_array(ch, seperators, 10)) {
+			ch2 = fin.peek();
+			if (ch2 == '%') {
+				cout << ch << ch2 << " seperator\n";
+				fout << ch << ch2 << " seperator\n";
+				fin.get();
+				return "seperator";
+			}
+			else if (ch2 == ']') { //handles *]
+				cout << ch << ch2 << " seperator\n";
+				fout << ch << ch2 << " seperator\n";
+				fin.get();
+				return "seperator";
+			}
+			else if (ch2 == '*') { //handles [*
+				cout << ch << ch2 << " seperator\n";
+				fout << ch << ch2 << " seperator\n";
+				fin.get();
+				char seeker;
+				while(seeker != '*'){
+					fin.get(seeker);
+				}
+				fin.unget();
+				return "seperator";
+			}
+			else if (ch == '*'){ //Since * can be an operator or beginning of sep, we need this to handle it being an op.
+				cout << ch << " operator\n";
+				fout << ch << " operator\n";
+				return "operator";
+			}
+			else {
+				cout << ch << " seperator\n";
+				fout << ch << " seperator\n";
+				return "seperator";
+			}
+		}
+
+		else if (in_array(ch, operators, 8)) {
+			ch2 = fin.peek();
+			if (ch2 == '>') {
+				cout << ch << ch2 << " operator\n";
+				fout << ch << ch2 << " operator\n";
+				fin.get();
+				return "operator";
+			}
+			else if (ch2 == '=') {
+				cout << ch << ch2 << " operator\n";
+				fout << ch << ch2 << " operator\n";
+				fin.get();
+				return "operator";
+			}
+			else {
+				cout << ch << " operator\n";
+				fout << ch << " operator\n";
+				return "operator";
+			}
+		}
+	}
+	//fout.close();
+  //fin.close();
+	return "$";
+}
+void LexerDone(){
+	fin.close();
+}
 //Beginning of rules
 bool APrime(){//the rule that augments the grammar. Just calls A. Not sure if it needs to check anything.
   if (A()){
@@ -11,11 +253,12 @@ bool APrime(){//the rule that augments the grammar. Just calls A. Not sure if it
   return false;
 }
 bool A(){
+  ch3 = ch + ch2;
   if(B()){
-    if(Lexer() == "seperator" && ch + next == "%%"){
+    if(Lexer() == "seperator" && ch3 == "%%"){
       if(J()){
         if(N()){
-          if(Lexer() == "seperator" && ch + next == "%%"){
+          if(Lexer() == "seperator" && ch3 == "%%"){
             return true;
           }
         }
@@ -168,7 +411,7 @@ bool N(){
   return false;
 }
 bool O(){
-  if(P(){
+  if(P()){
 
   }
   else if(Q()){
@@ -215,7 +458,7 @@ bool Q(){
 }
 bool R(){
   if(Lexer() == "keyword" && keyword == "if"){
-    if(Lexer() == "seperator" && ch == '('{
+    if(Lexer() == "seperator" && ch == '('){
       if(W()){
         if(Lexer() == "seperator" && ch == ')'){
           if(O()){
@@ -229,7 +472,7 @@ bool R(){
     return false;
   }
   else if(Lexer() == "keyword" && keyword == "if"){
-    if(Lexer() == "seperator" && ch == '('{
+    if(Lexer() == "seperator" && ch == '('){
       if(W()){
         if(Lexer() == "seperator" && ch == ')'){
           if(O()){
@@ -309,7 +552,8 @@ bool W(){
   return false;
 }
 bool X(){
-  if (Lexer() == "operator" && (ch + next == "==" || ch + next == "/=" || ch + next == "<=" || ch + next == "=>" || ch == '<' || ch == '>')){
+  ch3 = ch + ch2;
+  if (Lexer() == "operator" && (ch3 == "==" || ch3 == "/=" || ch3 == "<=" || ch3 == "=>" || ch == '<' || ch == '>')){
     return true;
   }
   return false;
